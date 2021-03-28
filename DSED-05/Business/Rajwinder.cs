3d2a8815887f26a3487c3c4b2c13b4c83b37085f
Rajwinder Singh <rsingh9083@gmail.com>
2021-03-28 20:24:09 +1300

    Add project files.

---
+++ DSED-05/Business/Rajwinder.cs
@@ -0,0 +1,20 @@
+﻿namespace DSED_05.Business
+{
+    using System.Drawing;
+
+    /// <summary>
+    /// Rajwinder Class.
+    /// </summary>
+    internal class Rajwinder : Punter
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Rajwinder"/> class.
+        /// </summary>
+        public Rajwinder()
+        {
+            Name = "Rajwinder";
+            Cash = 50;
+            Color = Color.Blue;
+        }
+    }
+}
